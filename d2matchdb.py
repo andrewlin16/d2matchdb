a79b8880ec92382a7508a6cae2043b17f11b0053
Andrew Lin <andrewlin16@gmail.com>
2014-12-21 18:11:22 -0600

    Add store, fix bugs

--- d2matchdb.py
+++ d2matchdb.py
@@ -3,10 +3,12 @@
 # API details (may be old?): https://wiki.teamfortress.com/wiki/WebAPI#Dota_2
 
 # imports
+from functools import reduce
 import json
 import os.path
 import requests
 import sqlite3
+import sys
 import time
 
 # settings constants
@@ -88,7 +90,7 @@ PLAYER_ATTRS = {"kills": "kills", "deaths": "deaths", "assists": "assists", "gol
 
 def fill_in(row_obj, player, prefix):
 	for attr in PLAYER_ATTRS:
-		row_obj[prefix + PLAYER_ATTRS.get(attr)] = players.get(attr)
+		row_obj[prefix + PLAYER_ATTRS.get(attr)] = player.get(attr)
 
 def combine_players(a, b):
 	val = dict()
@@ -105,18 +107,19 @@ def process_match(cur, match):
 	print("Found match " + str(match_id))
 
 	# only get match if it does not already exist in db
-	cur.execute("SELECT 1 FROM matches WHERE id = ?", match_id)
+	cur.execute("SELECT 1 FROM matches WHERE id = ?", (match_id,))
 	if (cur.fetchone() is None):
 		# get match details
-		qs = {"account_id": ACCOUNT_ID, "key": API_KEY, "date_min": last_time + 1}
+		qs = {"key": API_KEY, "match_id": match_id}
 		rate_limit()
-		match_details = requests.get(HISTORY_URL, params=qs).json().get("result")
+		match_details = requests.get(DETAILS_URL, params=qs).json().get("result")
 
+		# get player + team details
 		all_players = match_details.get("players")
 		player = next(filter(lambda o: o.get("account_id") == ACCOUNT_ID, all_players))
 		player_team = is_dire(player)
-		our_team = reduce(combine_players, filter(lambda p: is_dire(p) == player_team, players));
-		their_team = reduce(combine_players, filter(lambda p: is_dire(p) != player_team, players));
+		our_team = reduce(combine_players, filter(lambda p: is_dire(p) == player_team, all_players))
+		their_team = reduce(combine_players, filter(lambda p: is_dire(p) != player_team, all_players))
 
 		# fill in row object with fields
 		row_obj = dict()
@@ -131,6 +134,11 @@ def process_match(cur, match):
 		row_obj["start_time"] = match_details.get("start_time")
 		row_obj["game_mode"] = match_details.get("game_mode")
 		row_obj["ranked"] = match_details.get("lobby_type") == RANKED_LOBBY_TYPE
+
+		# store in db
+		sql_query = "INSERT INTO matches ({0}) VALUES ({1})".format(','.join(row_obj.keys()), ','.join('?'*len(row_obj)))
+		cur.execute(sql_query, tuple(row_obj.values()))
+		print("Stored " + str(match_id) + " in db")
 
 def process_match_history(cur, history):
 	# throw exception is status is not OK
