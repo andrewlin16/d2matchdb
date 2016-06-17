bf477ebca2f46c633bbc290ee592b63c3a92fedc
Andrew Lin <andrewlin16@gmail.com>
2016-06-17 02:26:09 -0700

    Add a "pretty print shell" for the match DB

---
+++ ppshell.py
@@ -0,0 +1,68 @@
+# Dota 2 local DB pretty-print shell
+
+import d2mdb_common as common
+import sqlite3
+import sys
+import time
+
+heroes = dict()
+
+# a bunch of lambdas to convert from values in the DB to proper representations
+FIELD_CONV = {
+	"hero": (lambda x: "%s (%d)" % (heroes[x], x)),
+	"team": (lambda x: common.TEAMS[x]),
+	"won": bool,
+	"start_time": time.ctime,
+	"game_mode": (lambda x: common.GAME_MODES[x] if x in common.GAME_MODES else "? (%s)" % x),
+	"ranked": bool,
+}
+
+def print_match(row):
+	for k in row.keys():
+		label = common.DB_FIELD_NAMES[k] if k in common.DB_FIELD_NAMES else k
+		v = row[k]
+		value = FIELD_CONV[k](v) if k in FIELD_CONV else v
+		print("%s: %s" % (label, value))
+
+def main():
+	global heroes
+
+	# check command line arguments
+	if len(sys.argv) < 2:
+		print("Usage: %s <db file>" % sys.argv[0])
+		return
+
+	hero_db = sqlite3.connect(common.HEROES_DB_FILE)
+	hero_db_cur = hero_db.cursor()
+	heroes = hero_db_cur.execute("SELECT id, name FROM heroes").fetchall()
+
+	heroes = dict(heroes)
+
+	db_file = sys.argv[1]
+	db = sqlite3.connect(db_file)
+	db.row_factory = sqlite3.Row
+	cur = db.cursor()
+
+	while True:
+		try:
+			sql = input("> ")
+			rows = cur.execute(sql)
+			num_results = 0
+			for row in rows:
+				print_match(row)
+				print()
+				num_results = num_results + 1
+			print("%s result(s)" % num_results)
+		except EOFError:
+			print("Bye")
+			break
+		except KeyboardInterrupt:
+			print()
+			pass
+		except sqlite3.OperationalError as sqlerror:
+			print("? - %s" % sqlerror)
+
+	db.close()
+
+if __name__ == '__main__':
+	main()
